3796cb4c5b8ea727779f5d19c6bb6510670f9edf
mikewhiteman <michaelswhiteman@gmail.com>
2021-05-04 01:14:27 -0400

    deployment for elastic beanstalk

--- application.py
+++ application.py
@@ -194,4 +194,4 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    (application.run(host='0.0.0.0', debug=True))
+    (application.run(host='0.0.0.0', debug=False))
